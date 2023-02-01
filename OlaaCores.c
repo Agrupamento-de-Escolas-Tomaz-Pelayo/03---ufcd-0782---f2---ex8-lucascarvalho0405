3215a06b0314b1c94bfabdd545516426943605e6
lucascarvalho0405 <a31559@tomazpelayo.com>
2023-02-01 14:45:14 +0000

    F2-ex8

--- OlaaCores.c
+++ OlaaCores.c
@@ -1,16 +1,33 @@
 // Resolva neste ficheiro o seu exercício
-#include<stdio.h>
-#include<windows.h>
+#include <stdio.h>
+#include <windows.h>
+
 int main()
 {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
-	printf("Ola Mundo! \n");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);   
+	printf(" #### ");
 	SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
-	printf("Ola Mundo! \n");
+	printf(" #    ");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+	printf("    #   \n");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE); 
+	printf("#    #");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+	printf(" #     ");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+	printf("  # #   \n");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE); 
+	printf("#    #");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+	printf(" #     ");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+	printf(" #####  \n");
 	SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
-	printf("Ola Mundo! \n");
-	return(0);
-
+	printf(" #### ");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+	printf(" ##### ");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+	printf("#     # \n");
+      return(0);
 }
-
